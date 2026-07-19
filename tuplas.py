676990cb1be42024bdbca1493a48779e552be2d0
jainerchocue <johanychocue@gmail.com>
2026-07-19 16:16:11 -0500

    En proceso

---
+++ tuplas.py
@@ -0,0 +1,10 @@
+# ✨ ¡Hola! Por el momento este archivo está vacío, ya que vamos paso a paso.
+# 
+# Te invito a revisar los otros archivos del repositorio donde ya voy subiendo 
+# varios códigos de ejemplo para que puedas ir practicando y estudiando.
+# 
+# Si te gusta este material, no olvides darme un "Like" para apoyar el proyecto:
+# ⭐ Aquí en GitHub: Haz clic en el botón de la estrellita (Star).
+# ❤️ En TikTok: Regálame un corazoncito en los videos.
+# 
+# ¡Muchas gracias por aprender tecnología conmigo y ser parte de esta comunidad! 🙌
